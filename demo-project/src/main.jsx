90709a10a6532b3b4d386116e43d828b53082ccf
RushikeshGhodke <rushikeshghodke7455@gmail.com>
2024-09-26 22:45:18 +0530

    router commit

--- demo-project/src/main.jsx
+++ demo-project/src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Home from './component/Home.jsx'
+import Layout from './Layout.jsx'
+import Contact from './component/Contact.jsx'
+import About from './component/About.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+    </Route>
+  )
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+
 )
